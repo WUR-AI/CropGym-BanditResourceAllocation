d9d9cc5f8ec7f00853ccb253e23475e49357d085
hbja <hbja96@gmail.com>
2025-09-09 10:16:42 +0200

    rename script; added some hyperparams

--- scripts/train_resource_allocation.py
+++
@@ -1,8 +0,0 @@
-import cropgymzoo
-
-import numpy as np
-import argparse
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
---
+++ scripts/training_resource_allocation.py
@@ -0,0 +1,22 @@
+import argparse
+
+from cropgymzoo.train_allocator import train_allocator
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=107)
+
+    # Meta stuff
+    parser.add_argument("--use_model", action='store_true')
+    parser.add_argument("--model_dir", type=str, default='MLP_PPO')
+    parser.add_argument("--rounds", type=int, default=1000)
+    parser.add_argument("--bandit_lr", type=float, default=5e-3)
+    parser.add_argument("--bandit_epochs", type=int, default=50)
+    parser.add_argument("--action_candidate_length", type=int, default=2048)
+    parser.set_defaults(
+        use_model=True,
+    )
+    args = parser.parse_args()
+
+    train_allocator(args)
